a947b65b03f844773a93782aa2f699c2ba1d2d76
Makoto Yano <yan133@gmail.com>
2020-07-21 17:10:27 +0900

    sassをビルドできるようにするところまで完成。

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './src/view/main.jsx',
   mode: 'development',
   output: {
-    path: path.resolve(__dirname, 'public/view/js'),
-    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public/view'),
+    filename: 'js/bundle.js',
   },
   module: {
     rules: [
@@ -20,10 +21,27 @@ module.exports = {
           ],
         },
       },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: { url: false },
+          },
+          'sass-loader',
+        ],
+      },
     ],
   },
   resolve: {
     modules: [__dirname, 'node_modules'],
-    extensions: ['.js', '.jsx', '.es'],
+    extensions: ['.js', '.jsx', '.es', '.scss'],
   },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'css/style.css',
+    }),
+  ],
 };
