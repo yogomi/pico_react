e8ba5a8932486e5dcc5000f82d35dd9c23c7ca78
Makoto Yano <yan133@gmail.com>
2020-07-21 19:17:04 +0900

    Revert "sassをビルドできるようにするところまで完成。"
    This reverts commit a947b65b03f844773a93782aa2f699c2ba1d2d76.

--- src/view/main.jsx
+++ src/view/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './sass/style.scss';
 
 function Root() {
   return (
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,11 @@
 const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './src/view/main.jsx',
   mode: 'development',
   output: {
-    path: path.resolve(__dirname, 'public/view'),
-    filename: 'js/bundle.js',
+    path: path.resolve(__dirname, 'public/view/js'),
+    filename: 'bundle.js',
   },
   module: {
     rules: [
@@ -21,27 +20,10 @@ module.exports = {
           ],
         },
       },
-      {
-        test: /\.scss$/,
-        exclude: /node_modules/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          {
-            loader: 'css-loader',
-            options: { url: false },
-          },
-          'sass-loader',
-        ],
-      },
     ],
   },
   resolve: {
     modules: [__dirname, 'node_modules'],
-    extensions: ['.js', '.jsx', '.es', '.scss'],
+    extensions: ['.js', '.jsx', '.es'],
   },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: 'css/style.css',
-    }),
-  ],
 };
